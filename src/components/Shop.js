ae273232a985ae64ae2e8d35f84579a3cf21bb89
Almeida-91 <aplalmeida91@outlook.com>
2022-10-20 08:04:22 +0100

    Add item list to shop component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Shop = () => {
+    const item1 = {
+        name: "Generic Item",
+        Price: "19,99$",
+    }
+
+    const item2 = {
+        name: "Less Generic Item",
+        Price: "24,99$",
+    }
+
+    const item3 = {
+        name: "Not a Generic Item",
+        Price: "29,99$",
+    }
+
+    const [itemList, setItemList] = useState([item1,item2,item3]);
+
+
     return (
-        <div></div>
+        <div>
+        </div>
     )
 }
 
