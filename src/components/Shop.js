96816a5ecd5e7e976dfbf7565636b4821b55140b
Almeida-91 <aplalmeida91@outlook.com>
2022-10-20 08:07:13 +0100

    List shop items in shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,24 +3,29 @@ import React, { useState } from "react";
 const Shop = () => {
     const item1 = {
         name: "Generic Item",
-        Price: "19,99$",
+        price: "19,99$",
     }
 
     const item2 = {
         name: "Less Generic Item",
-        Price: "24,99$",
+        price: "24,99$",
     }
 
     const item3 = {
         name: "Not a Generic Item",
-        Price: "29,99$",
+        price: "29,99$",
     }
 
     const [itemList, setItemList] = useState([item1,item2,item3]);
-
+    const listItems = itemList.map((item)=>
+    <div>
+        <li>{item.name}</li>
+        <li>{item.price}</li>
+    </div>)
 
     return (
         <div>
+            {listItems}
         </div>
     )
 }
