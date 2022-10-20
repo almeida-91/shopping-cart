280e84409b69e920f6862130d02d57fb54139a4f
Almeida-91 <aplalmeida91@outlook.com>
2022-10-20 19:17:43 +0100

    Add working shopping cart

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -16,26 +16,55 @@ const Shop = () => {
     price: "29,99$",
   };
 
+  const addItemToCart = (item, quant) => {
+    if (quant < 1) {
+      alert("Quantity must be greater than 1");
+      return;
+    }
+    let itemToCart = {
+      cartItem: item,
+      itemQuantity: quant,
+    };
+    setShopCart(shopCart.concat(itemToCart));
+    setQuantity(0);
+  };
+
+  const handleQuantityChange = (e) => {
+    if (e.target.value < 1) return;
+    setQuantity(e.target.value);
+  };
+
   const [itemList, setItemList] = useState([item1, item2, item3]);
+  const [quantity, setQuantity] = useState(0);
+
   const listItems = itemList.map((item) => (
     <div>
       <li>{item.name}</li>
       <li>{item.price}</li>
       <label htmlFor="numberOfItems">How many?</label>
-      <input type="number" name="numberOfItems"></input>
-      <button>Add to cart</button>
+      <input
+        type="number"
+        name="numberOfItems"
+        pattern="[1-9]"
+        defaultValue={1}
+        onChange={handleQuantityChange}
+      ></input>
+      <button
+        onClick={() => {
+          addItemToCart(item, quantity);
+        }}
+      >
+        Add to cart
+      </button>
     </div>
   ));
 
-  const [shopCart, setShopCart] = useState({
-    numberOfItemsInCart: 0,
-    items: [],
-  });
+  const [shopCart, setShopCart] = useState([]);
 
   return (
     <div>
-        <div>Shopping Cart: {shopCart.numberOfItemsInCart}</div>
-        <div>{listItems}</div>
+      <div>Shopping Cart: {shopCart.length}</div>
+      <div>{listItems}</div>
     </div>
   );
 };
