07a4363764d4b4e4403774641528da7d90a481e3
Almeida-91 <aplalmeida91@outlook.com>
2022-10-20 07:51:14 +0100

    Add homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import RouteSwitch from "./components/RouteSwitch";
 
 function App() {
   return (
-    <div></div>
+
+    <div>
+    <h1> Generic Shop</h1>
+
+    <nav>
+        <a>Home</a> |
+        <a> Shop</a>
+    </nav>
+    <RouteSwitch />
+</div>
   );
 }
 
